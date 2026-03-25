38d97c7c726ce7edbaa8be78d969c2f3c78877d0
saharbagheri <sahar.bagheri1@ucalgary.ca>
2026-03-25 10:11:14 -0600

    Update pipeline_sbatch.sh

--- pipeline_sbatch.sh
+++ pipeline_sbatch.sh
@@ -24,7 +24,7 @@ conda activate snakemake
 
 echo "started at: `date`"
 
-snakemake --latency-wait 100 --rerun-incomplete --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &>> $log_dir/$log_file
+snakemake --rerun-triggers mtime --latency-wait 100 --rerun-incomplete --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &>> $log_dir/$log_file
 
 output_dir=$(grep "output_dir" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
 list_files=$(grep "sampletable" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
